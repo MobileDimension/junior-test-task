89b791109c3ba0ae1fedd09f12fb14fd41802c83
lizzeykudi <lizzeykudi@gmail.com>
2017-11-29 20:08:52 +0400

    task done

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -1,6 +1,7 @@
 package ru.buyanov.hunting;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  *  @author https://github.com/alex-on-java 03.02.2016
@@ -18,15 +19,15 @@ public class IntIterable implements Iterable<Integer> {
     }
 
     private class IntIterator implements Iterator<Integer> {
+        private int index = 0;
 
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+            return (index<backed.length) ;
         }
 
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+            if (!hasNext()) { throw new NoSuchElementException(); }
+            return backed[index++];
         }
 
         public void remove() {
