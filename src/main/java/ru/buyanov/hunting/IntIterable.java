ea5522f2bb32426a9691ecfcf5afed3a621ac44f
Denis Bogdanov <den_bogdan@mail.ru>
2018-07-31 21:20:04 +0300

    result

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -3,7 +3,7 @@ package ru.buyanov.hunting;
 import java.util.Iterator;
 
 /**
- *  @author https://github.com/alex-on-java 03.02.2016
+ * @author https://github.com/alex-on-java 03.02.2016
  */
 public class IntIterable implements Iterable<Integer> {
     int[] backed;
@@ -19,14 +19,17 @@ public class IntIterable implements Iterable<Integer> {
 
     private class IntIterator implements Iterator<Integer> {
 
+        private int index = 0;
+
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+
+            return index < backed.length;
+
         }
 
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+
+            return backed[index++];
         }
 
         public void remove() {
