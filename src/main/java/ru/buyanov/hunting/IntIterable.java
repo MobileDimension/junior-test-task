565bfa91274cdffb7ba331b628e4604fa32a011e
Sea Wind <lab317@yandex.ru>
2018-08-09 22:35:32 +0300

    added the two methods implementation

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -1,34 +1,38 @@
 package ru.buyanov.hunting;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  *  @author https://github.com/alex-on-java 03.02.2016
  */
 public class IntIterable implements Iterable<Integer> {
-    int[] backed;
-
+    private int[] backed;
 
     public IntIterable(int[] backed) {
         this.backed = backed;
     }
-
+    
+    @Override
     public Iterator<Integer> iterator() {
         return new IntIterator();
     }
 
     private class IntIterator implements Iterator<Integer> {
-
+    	private int index = 0;
+    	
+    	@Override
         public boolean hasNext() {
-            //TODO: You task is implement this method
-            return false;
+            return index<backed.length;
         }
-
+    	
+    	@Override
         public Integer next() {
-            //TODO: You task is implement this method
-            return null;
+    		if (hasNext()) return backed[index++];
+    		throw new NoSuchElementException();
         }
-
+    	
+    	@Override
         public void remove() {
             throw new IllegalStateException("Could not remove from array");
         }
